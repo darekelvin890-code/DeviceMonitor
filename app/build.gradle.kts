644fc5e909d905f41271d8e517009d1874b44bd9
darekelvin890-code <darekelvin890@gmail.com>
2026-05-15 13:10:29 +0100

    Add nanohttpd dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.google.android.material:material:1.11.0")
+    implementation 'org.nanohttpd:nanohttpd:2.3.1'
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
